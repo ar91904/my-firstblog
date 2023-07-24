586b9919a441631e3ad4338c59f61dbbc6125fd5
Amelie <amelieredfern@gmail.com>
2023-07-24 17:26:26 +0100

    Pagination was attempted

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,22 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django .core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    object_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    page = request.GET.get('page')
+    paginator = Paginator(object_list, 3) # 3 posts per page
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'page': page})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
